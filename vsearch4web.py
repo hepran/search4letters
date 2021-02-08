896aac7dfeb34e0bb0a72b70e107745ae9ee53e1
hepran <hepran@protonmail.ch>
2021-02-08 14:31:54 +0700

    Format the layout of the log data.

--- vsearch4web.py
+++ vsearch4web.py
@@ -16,18 +16,12 @@ app = Flask(__name__)
 def log_request(req: 'flask_request', res: str) -> None:
     """
     Store and manipulate the search4letters's data.
-    When this function invokes, the "req" argument is assigned to the current
-    Flask request object, while the "res" argument is assigned the results from
-    calling search4letters.
-    This function also appends the value of "req" and "res" to a log file named
-    "vsearch.log".
-    
-    "...str(dir(req))..." produces a list of "req", then that list is converted
-    to string, and then that string is saved to the log file along with "res"
-    value.
+    When this function invokes, it displays the formatted out as follows:
+        [data from webapp]|[IP address]|[browser ID]|[result]
+    and then appends to a log file named vsearch.log.
     """
     with open('vsearch.log', 'a') as log:
-        print(str(dir(req)), res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
     
 @app.route('/search4', methods=['POST'])
 def do_seacrh() -> 'html':
@@ -51,25 +45,16 @@ def do_seacrh() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    """
-    Show the homepage or the page to entry what a user wants to search.
-    """
+    """Show the homepage or the page to entry what a user wants to search."""
     return render_template('entry.html',
                             the_title='Welcome to search4letters on the web!')
     
 @app.route('/viewlog')
 def view_the_log()-> str:
-    """
-    Show the log.
-    
-    See the escape function at the return statement.
-    That function makes the "req" argument in log_request() function which
-    results HTML behaves like a string, just like the "res" argument.
-    By that, it enables web browser to display the result of "req" and "res".
-    """
+    """Show the log."""
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+        contents = log.readlines()
+    return escape(''.join(contents))
 
 if __name__ == '__main__':
     app.run(debug=True)
